0c073277f6910cfd035bd7bc520cb6622ecdeabd
arahansa <arahansa@naver.com>
2017-08-27 22:00:57 +0900

    add log

--- src/main/java/com/example/arahansa/ArahansaApplication.java
+++ src/main/java/com/example/arahansa/ArahansaApplication.java
@@ -1,16 +1,21 @@
 package com.example.arahansa;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @SpringBootApplication
 public class ArahansaApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ArahansaApplication.class, args);
+
+		ConfigurableApplicationContext run = SpringApplication.run(ArahansaApplication.class, args);
+		log.info("app here : {}", run);
 		System.out.println("=== hello world === ");
 	}
 
